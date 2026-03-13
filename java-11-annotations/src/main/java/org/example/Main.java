95504cbe5a816d40d411b8c80c29f47132d0d695
Bruno Scherer <bruno.raphael96@gmail.com>
2026-03-13 00:14:08 -0700

    java-11-annotations: add annotation example

--- java-11-annotations/src/main/java/org/example/Main.java
+++ java-11-annotations/src/main/java/org/example/Main.java
@@ -1,17 +1,37 @@
 package org.example;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface MagicSpell {
+    String value();
+}
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    @MagicSpell("Abracadabra!")
+    public void castSpell() {
+        System.out.println("Spell cast!");
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        try {
+            Class<?> clazz = Main.class;
+            Method method = clazz.getMethod("castSpell");
+            MagicSpell annotation = method.getAnnotation(MagicSpell.class);
+            if (annotation != null) {
+                System.out.println("Reading annotation: " + annotation.value());
+            }
+            Main instance = new Main();
+            method.invoke(instance);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
